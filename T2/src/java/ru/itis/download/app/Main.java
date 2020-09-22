f6a1d15010bbee5b252dd320237f38b9c9e8d0df
EkaterinaTR <ekaterinatsr@gmail.com>
2020-09-22 18:06:00 +0300

    new comments in main

--- T2/src/java/ru/itis/download/app/Main.java
+++ T2/src/java/ru/itis/download/app/Main.java
@@ -6,8 +6,7 @@ import ru.itis.download.utils.Task;
 import ru.itis.download.utils.Args;
 import com.beust.jcommander.JCommander;
 /**problem : for git ';' is end of command.In my program ';' is "LLL" */
-/** for exampl java -jar download.jar --mode=one-thread --files=https://zveri.guru/images/232854/sobaka-schenok-medvezhonok.jpgLLLht tps://i.pinimg.com/236x/dd/4c/8c/dd4c8c22d3a9bb02068a0d13162b1e74--white-bunnies-white-rabbits.jpg --folder=C:\\Users\\
-User\\Pictures\\forApp*/
+/** java -jar download.jar --mode=m --count=2 --files=https://zveri.guru/images/232854/sobaka-schenok-medvezhonok.jpgLLLhttps://i.pinimg.com/236x/dd/4c/8c/dd4c8c22d3a9bb02068a0d13162b1e74--white-bunnies-white-rab bits.jpgLLLhttp://risovat-legko.com/wp-content/uploads/malenkie-kartinki-dlja-srisovki-1.jpg --folder=C:\\Users\\User\\Pictures\\forApp*/
 
 
 public class Main {
